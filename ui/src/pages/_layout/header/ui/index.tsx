0e1ea623d3b01c5486a9a99e4ad0b75e70b0d09b
Serafim Gerasimov <serafimgerasimov44@gmail.com>
2024-10-31 23:47:32 +0500

    fix: back button position

--- ui/src/pages/_layout/header/ui/index.tsx
+++ ui/src/pages/_layout/header/ui/index.tsx
@@ -1,14 +1,18 @@
-import { Flex, Layout } from "antd";
+import { Col, Layout, Row } from "antd";
 import { BackButton } from "./backButton";
 import { AppVersion } from "widgets/appVersion";
 
 export function AppHeader() {
   return (
     <Layout.Header className="flex align-middle">
-      <BackButton />
-      <Flex className="w-full h-full" justify="end" align="center">
-        <AppVersion />
-      </Flex>
+      <Row className="w-full h-full align-middle">
+        <Col span={5}>
+          <BackButton />
+        </Col>
+        <Col offset={14} span={5}>
+          <AppVersion />
+        </Col>
+      </Row>
     </Layout.Header>
   );
 }
