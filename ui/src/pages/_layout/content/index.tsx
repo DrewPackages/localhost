be04bf93d880d4b3a77ccbc4a5d0a4906652be88
Serafim Gerasimov <serafimgerasimov44@gmail.com>
2024-11-01 00:17:27 +0500

    feat: feedback button

--- ui/src/pages/_layout/content/index.tsx
+++ ui/src/pages/_layout/content/index.tsx
@@ -1,10 +1,25 @@
-import { Layout } from "antd";
-import { PropsWithChildren } from "react";
+import { EditOutlined } from "@ant-design/icons";
+import { FloatButton, Layout } from "antd";
+import { PropsWithChildren, useCallback } from "react";
+import service from "localhostService";
 
 const { Content } = Layout;
 
 interface AppContentProps {}
 
 export function AppContent({ children }: PropsWithChildren<AppContentProps>) {
-  return <Content className="p-4 min-h-screen">{children}</Content>;
+  const onFeedbackClick = useCallback(() => {
+    service.openUrlInBrowser("https://forms.gle/ggRkx4YmzdosSeuU7");
+  }, []);
+  return (
+    <Content className="p-4 min-h-screen">
+      {children}
+      <FloatButton
+        type="primary"
+        tooltip="Feedback"
+        icon={<EditOutlined />}
+        onClick={onFeedbackClick}
+      />
+    </Content>
+  );
 }
